5a446f872fd30c59e4820603d1d1a7da5c30ae33
Ayikson <barnabasayiki@gmail.com>
2023-02-24 03:28:52 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,19 @@
 
 /**
  * more_numbers - print more numbers
- * Return: no return
  */
 
 void more_numbers(void)
 {
-
 int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
-			_Putchar(j % 10 + '0');
+			_Putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
